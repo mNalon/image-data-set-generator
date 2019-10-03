15dec99fa7dce63cad9ee61cd923eace8137467b
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2019-10-03 12:44:01 -0300

    Deal with exception when there is no initial temp_path

--- generate_image_data_set.py
+++ generate_image_data_set.py
@@ -8,7 +8,11 @@ from save_image_to_data_set import save_image_to_data_set
 print("initializing the images dataset creation")
 
 print ("erasing all data on %s" % (__TEMP_PATH))
-shutil.rmtree(__TEMP_PATH)
+try:
+  shutil.rmtree(__TEMP_PATH)
+except Exception as e:
+  print('it was not possible erase the temp_path')
+  print(e)
 
 entities = extract_entities_from_csv(TITLES_DATA_SET_CSV_URL)
 
